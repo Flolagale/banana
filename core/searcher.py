e46fb38d6dcaea481fe14e2187d0ac69c2865616
Florent Galland <florent.galland@gmail.com>
2012-05-25 14:35:08 +0200

    Move BlocProcessor to its own module.

--- core/searcher.py
+++ core/searcher.py
@@ -15,7 +15,6 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with banana.  If not, see <http://www.gnu.org/licenses/>.
-from index import Index, BlobProcessor
 import logging
 import math
 import os
